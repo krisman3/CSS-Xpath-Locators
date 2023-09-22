ec27e858ee8d95bfac2ac40314294b1143e08254
KIliev <kriskokz@gmail.com>
2023-09-23 00:50:28 +0300

    My part of the homework for the CSS/XPath selectors.

---
+++ src/main/java/org/skillo/automation/test/Main.java
@@ -0,0 +1,35 @@
+package org.skillo.automation.test;
+
+public class Main {
+    public static void main(String[] args) {
+        // On home page - (Didn't read the homework assignment)
+
+        // $("#nav-link-new-post")         - New post
+        // $("#nav-link-profile")            - Profile button
+        // $("#nav-link-home")       - Home button
+        // $("#homeIcon")   - Home icon (ISkillo)
+        // $("ul.navbar-nav")    - All three buttons "Home/Profile/New post"
+        // $("button.btn")     -  Unfollow button on first post
+        // $("i.far")    -   The heart button (like button under post)
+        // $(".form-control")   - Search bar
+        // $x("//li/a/i") - Sign Out button
+        // $("i.fas")  - Sign Out button
+        // $x("//i[@class='fas fa-sign-out-alt fa-lg']")  - Sign out button
+        // $x("//div/button[@class='btn btn-primary']")  -  Every single Unfollow button
+
+        // Actual homework assignment
+
+        // $x("//div[@class='uploadfilecontainer']")   - The dropdown field
+        // $("#choose-file")   - Browse button
+        // $x("//div/input[@class='form-control mb-4 ng-pristine ng-invalid ng-touched']")  - Enter your post caption field
+        // $x('//input[@placeholder="Upload Picture"]')      - Text "Upload Picture"
+        // $("#create-post")  -  Submit button
+        // $x("//i[@class='fas fa-sign-out-alt fa-lg']")       - Sign out button
+        // $x("//form/input")      -   Search bar
+        // $x("//form/input[@placeholder='Search']")   - Alternative Search Bar
+        // $("#nav-link-new-post")         - New post
+        // $("#nav-link-profile")            - Profile button
+        // $("#nav-link-home")       - Home button
+
+    }
+}
