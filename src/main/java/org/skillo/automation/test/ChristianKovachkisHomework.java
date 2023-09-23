3c9b3e68c42901eb654da571ba83bf0f18b16dc1
Christian Kovachki <christian8251@gmail.com>
2023-09-23 11:21:01 +0300

    CSS and XPath Homework From Christian Kovachki

---
+++ src/main/java/org/skillo/automation/test/ChristianKovachkisHomework.java
@@ -0,0 +1,30 @@
+package org.skillo.automation.test;
+
+public class ChristianKovachkisHomework {
+    /*
+
+    Upload File Container (Drop an image)
+    $$('.uploadfilecontainer')
+    $x('//div[@class="uploadfilecontainer"]')
+
+    Browse Button
+    $$('#choose-file')
+    $x('//span/button[@class="browse btn btn-primary input-lg"]')
+    $x('//span/button[@id="choose-file"]')
+
+    Input Field
+    $$('input[name = "caption"][placeholder="Enter you post caption here"]')
+    $x('//input[@placeholder="Enter you post caption here"]')
+    $x('//input[@name="caption"]')
+
+    Toggle Switch
+    $$('.custom-control-label')
+    $x('//label[@class="post-status-label custom-control-label active"]')
+
+    Submit Button
+    $$('#create-post')
+    $x('//div/button[@id="create-post"]')
+    $x('//div/button[@class="btn btn-primary"]')
+
+    */
+}
